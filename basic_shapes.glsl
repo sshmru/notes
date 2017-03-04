c42bd65c75a17c3e996e84155273aeb88aec9c9f
Piotr Malinowski <sshmru@gmail.com>
2017-03-04 12:31:18 +0100

    Update basic_shapes.glsl

--- basic_shapes.glsl
+++ basic_shapes.glsl
@@ -2,9 +2,14 @@ float circle( vec2 uv, float radius )
 {
 	return 1.0 -length( uv / radius );
 }
-
+//x,y,x2,y2
 float rect(vec2 uv, vec4 rect){
     vec2 hv = step(rect.xy, uv) * step(uv, rect.zw);
+    return hv.x * hv.y;	
+}
+//x,y,w,h
+float rect(vec2 uv, vec4 rect){
+    vec2 hv = step(rect.xy, uv) * step(uv, rect.xy + rect.zw);
     return hv.x * hv.y;	
 }
 
