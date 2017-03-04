bea08cf0e5f5a8d7f18361d5a1f6a501404ddfda
Piotr Malinowski <sshmru@gmail.com>
2017-03-04 16:37:16 +0100

    Update basic_shapes.glsl

--- basic_shapes.glsl
+++ basic_shapes.glsl
@@ -4,6 +4,15 @@ float circle( vec2 uv, float radius )
 	return 1.0 -length( uv / radius );
 }
 
+//improves performance over sqrt based functions
+float circle(in vec2 _st, in float _radius){
+    vec2 dist = _st-vec2(0.5);
+	return 1.-smoothstep(_radius-(_radius*0.01),
+                         _radius+(_radius*0.01),
+                         dot(dist,dist)*4.0);
+}
+
+
 //x,y,r
 float circ(vec2 uv, vec2 center, float radius){
     return step(length(uv - center), radius);
