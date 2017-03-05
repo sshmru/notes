011036a314401e4cde9d81772654b7661b25dfc1
Piotr Malinowski <sshmru@gmail.com>
2017-03-05 20:21:20 +0100

    Update transformations.glsl

--- transformations.glsl
+++ transformations.glsl
@@ -1,3 +1,5 @@
+// [cos(angle),-sin(angle)] * [x]
+// [sin(angle),cos(angle)]    [y]
 mat2 rotate2d(float _angle){
     return mat2(cos(_angle),-sin(_angle),
                 sin(_angle),cos(_angle));
@@ -6,3 +8,12 @@ mat2 rotate2d(float _angle){
 vec2 st = gl_FragCoord.xy/u_resolution.xy;
 vec2 p =  st - vec2(0.5);
 p*= rotate2d(PI*0.1);
+
+
+// [Sx, 0] * [x]
+// [0, Sy]   [y]
+
+mat2 scale(vec2 _scale){
+    return mat2(_scale.x,0.0,
+                0.0,_scale.y);
+}
